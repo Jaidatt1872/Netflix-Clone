f4e0e2e89a3c70aece9c8abb6219d447bd9b2922
Jaidatt Kadam <jaidatt18@gmail.com>
2023-07-28 21:01:08 +0530

    node setup done

--- src/pages/Netflix.js
+++ src/pages/Netflix.js
@@ -6,19 +6,29 @@ import { FaPlay } from 'react-icons/fa';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 import { styled } from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { getGenres } from '../store';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMovies, getGenres } from '../store';
+import Slider from '../components/Slider';
 
 export default function Netflix() {
   
   const [isScrolled, setIsScrolled] = useState(false);
+  const genresLoaded = useSelector((state)=> state.netflix.genresLoaded);
+  const movies = useSelector((state)=> state.netflix.movies);
+  const genres = useSelector((state) => state.netflix.genres);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   
   useEffect(()=> {
       dispatch(getGenres());
-  }, [])
- 
+  }, []);
+
+  useEffect(() => {
+    if (genresLoaded) {
+        dispatch(fetchMovies({genres, type: "all"}));
+    }
+  }, [genresLoaded]);
+  
   window.onscroll = ()=> {
       setIsScrolled(window.scrollY === 0 ? false : true);
       return ()=> {window.onscroll = null};
@@ -43,6 +53,7 @@ export default function Netflix() {
                 </div>
             </div>
         </div>
+        <Slider movies={movies}/>
     </Container>
   )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -10,22 +10,71 @@ const initialState = {
 
 export const getGenres = createAsyncThunk("netflix/genres", async()=>{
     const {data: {genres}} = await axios.get(
-        (`${TMDB_BASE_URL}/genre/movie/list?api_key=${API_KEY}`)
+        ("https://api.themoviedb.org/3/genre/movie/list?api_key=e9d811bea5940b5918f82343dc8406a1")
     );
     return genres;  
 });
 
-export const fetchMovies = createAsyncThunk("netflix/trending", async ({type}, thunkApi)=> {
-    
-});
+const createArrayFromRawData = (array, moviesArray, genres)=> {
+    array.forEach((movie) => {
+        const movieGenres = [];
+        movie.genre_ids.forEach((genre)=> {
+            const name = genres.find(({ id }) => id === genre);
+            if(name) {
+                movieGenres.push(name.name)
+            };
+        });
+        if (movie.backdrop_path) {
+            moviesArray.push({
+                id: movie.id,
+                name: movie?.original_name ? movie.original_name : movie.original_title,
+                image: movie.backdrop_path,
+                genres: movieGenres.slice(0,3)
+            });
+        }
+    });
+}
+
+const getRawData = async (api, genres, paging = false)=> {
+    const moviesArray = [];
+    for (let i = 1;  moviesArray.length < 60 && i < 10; i++) {
+        const {data: {results}} = await axios.get(`${api}${paging ? `&page=${i}` : ""}`);
+        createArrayFromRawData(results, moviesArray, genres);
+    }
+    return moviesArray;
+}
+
+export const fetchDataByGenre = createAsyncThunk("netflix/genre",
+  async ({genre, type}, thunkAPI)=> {
+    const { 
+        netflix: { genres },    
+    } = thunkAPI.getState();
+    return getRawData(`https://api.themoviedb.org/3/discover/${type}?api_key=e9d811bea5940b5918f82343dc8406a1&with_genres=${genre}`, genres);
+  }
+); 
+
+export const fetchMovies = createAsyncThunk("netflix/trending", 
+  async ({type}, thunkAPI)=> {
+    const { 
+        netflix: { genres },    
+    } = thunkAPI.getState();
+    return getRawData(`${TMDB_BASE_URL}/trending/${type}/week?api_key=${API_KEY}`, genres, true);
+  }
+);
 
 const NetflixSlice = createSlice({
-    name: "netflix",
+    name: "Netflix",
     initialState,
     extraReducers: (builder)=> {
         builder.addCase(getGenres.fulfilled,(state,action)=> {
             state.genres = action.payload;
             state.genresLoaded = true;
+        });
+        builder.addCase(fetchMovies.fulfilled,(state,action)=> {
+            state.movies = action.payload;
+        });
+        builder.addCase(fetchDataByGenre.fulfilled,(state,action)=> {
+            state.movies = action.payload;
         });
     }
 });
